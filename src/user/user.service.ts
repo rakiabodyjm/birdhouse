333f7cc0f827d8c814dccbc9efcb09281dae7a9f
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-06 20:14:13 +0800

    Initialize User resource

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from 'src/user/entities/user.entity'
+import { SQLDateGenerator } from 'src/utils/SQLDateGenerator'
+import { Repository } from 'typeorm'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto)
+    await this.userRepository.save(user)
+    return user
+    // return 'This action adds a new user'
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(): Promise<User[]> {
+    const users = await this.userRepository.find()
+    return users
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<User> {
+    // return await this.userRepository.findByIds([id])[0]
+    return await this.userRepository.findOne(id)
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto | Partial<UpdateUserDto>,
+  ): Promise<User> {
+    const userQuery: User = await this.userRepository.findOne(id)
+    if (!userQuery) {
+      console.log('User Query', userQuery)
+      throw new Error('User not found')
+    }
+    const updatedUser: User = {
+      ...userQuery,
+      ...updateUserDto,
+      // updated_at: new Date(new SQLDateGenerator().timeNow().getSQLDate()),
+      updated_at: new SQLDateGenerator().timeNow().getSQLDateObject(),
+    }
+    const user = await this.userRepository.save(updatedUser)
+    return user
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async delete(id: string): Promise<User> {
+    const user = await this.userRepository.findOne(id)
+    const deleteResult = await this.userRepository.delete(id)
+    console.log(deleteResult)
+    return user
+  }
+
+  clear() {
+    this.userRepository.clear()
+    return {
+      message: 'Users succesfully Cleared',
+    }
   }
 }

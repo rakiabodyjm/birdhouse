a2faec0d4bd875c9d811231eb0947194fb2acd7a
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-06 20:14:18 +0800

    Initialize User resource

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsNotEmpty,
+  IsNumberString,
+  IsPhoneNumber,
+  MaxLength,
+} from 'class-validator'
+
+export class CreateUserDto {
+  @IsNotEmpty({
+    message: `First Name can't be empty`,
+  })
+  @MaxLength(30)
+  first_name: string
+
+  @IsNotEmpty({
+    message: `Last Name can't be empty`,
+  })
+  @MaxLength(30)
+  last_name: string
+
+  @IsNotEmpty({
+    message: 'Phone Number is Empty',
+  })
+  @IsPhoneNumber('PH', { message: 'Invalid Phone Number' })
+  phone_number: string
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { SQLDateGenerator } from 'src/utils/SQLDateGenerator'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  first_name: string
+
+  @Column()
+  last_name: string
+
+  @Column()
+  phone_number: string
+
+  @Column({ default: true })
+  active: boolean
+
+  @Column({
+    type: 'datetime',
+    default: new SQLDateGenerator().timeNow().getSQLDate(),
+  })
+  created_at: Date
+
+  @Column({
+    type: 'datetime',
+    default: new SQLDateGenerator().timeNow().getSQLDate(),
+    nullable: false,
+  })
+  updated_at: Date
+}

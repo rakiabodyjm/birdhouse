6ac64700a346cfcce02905e0d69693a1d2d0d63c
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-08 17:19:03 +0800

    Admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,31 +4,40 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
-import { User } from 'src/user/entities/user.entity'
 // import { MapIdService } from './map-id/map-id.service';
 // import { MapIdController } from './map-id/map-id.controller';
 // import { MapIdModule } from './map-id/map-id.module';
 import { MapIdsModule } from './map-ids/map-ids.module'
 import { DspModule } from './dsp/dsp.module'
+import { config } from 'dotenv'
+import { AdminModule } from './admin/admin.module'
+import { AuthModule } from './auth/auth.module'
 
+config()
 const additionalOptions = {
   type: 'mssql',
   host: '192.168.1.14',
   port: 1433,
-  username: 'sa',
-  password: 'rakiabodyjm4690',
+  username: 'realm1000/dito',
+  password: 'Oasis2089$',
   database: 'dito_db',
   synchronize: true,
-  logging: true,
-  entities: [User],
+  logging: false,
+  retryAttempts: 100,
   options: {
-    trustServerCertificate: true,
+    trustServerCertificate: process.env.NODE_ENV === 'development',
+  },
+  // entities: [User, MapId, Dsp],
+  autoLoadEntities: true,
+  pool: {
+    // idleTimeoutMillis: 100000,
+    // max: 14000,
+    // min: 0,
   },
 } as TypeOrmModuleOptions
 
 @Module({
   imports: [
-    UserModule,
     ConfigModule.forRoot({
       envFilePath: '.development.env',
     }),
@@ -43,6 +52,9 @@ const additionalOptions = {
     }),
     MapIdsModule,
     DspModule,
+    UserModule,
+    AdminModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],

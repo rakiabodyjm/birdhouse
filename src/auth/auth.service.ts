fde95c539cc9dcc2047100f7de099fc6273198c4
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-09 17:20:47 +0800

    Auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common'
+import { isEmail } from 'class-validator'
+import { LoginUserDto } from 'src/auth/dto/login.dto'
+import { User } from 'src/user/entities/user.entity'
+import { UserService } from 'src/user/user.service'
+import { Bcrypt } from 'src/utils/Bcrypt'
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UserService) {}
+  /**
+   *
+   * @param email email | username
+   * @param password password
+   */
+  async validateUser(email: string, password: string): Promise<User> {
+    let user: User | null
+    if (isEmail(email)) {
+      /**
+       * Email credentials to be used
+       */
+      user = await this.usersService.findByEmail(email)
+      // console.log('user from email', user)
+    } else {
+      // console.log('user from username', user)
+      user = await this.usersService.findByUsername(email)
+    }
+
+    if (!user) {
+      throw new Error(`User doesn't exist`)
+    }
+
+    const isPasswordCorrect = Bcrypt().isPasswordCorrect(
+      password,
+      user.password,
+    )
+    if (!isPasswordCorrect) {
+      throw new Error(`Password incorrect`)
+    }
+
+    return user
+  }
+
+  //   async getRole(id) {
+  //     const adminRoles: UserRoles[] = ['admin', 'dsp']
+  //   }
+}

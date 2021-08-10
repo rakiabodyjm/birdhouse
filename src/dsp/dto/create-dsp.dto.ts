d2340b58b0e7d84859a3a654d050c15e05057941
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-10 17:22:25 +0800

    DSP Module

--- src/dsp/dto/create-dsp.dto.ts
+++ src/dsp/dto/create-dsp.dto.ts
@@ -1,1 +1,44 @@
-export class CreateDspDto {}
+import { PartialType } from '@nestjs/mapped-types'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPhoneNumber,
+  IsString,
+  IsUUID,
+} from 'class-validator'
+import { Dsp } from 'src/dsp/entities/dsp.entity'
+import { ExistsInDb } from 'src/dsp/pipes/validation/ExistsInDb'
+import { NoDuplicateInDb } from 'src/dsp/pipes/validation/NoDuplicateInDb'
+import { MapId } from 'src/map-ids/entities/map-id.entity'
+import { CreateUserDto } from 'src/user/dto/create-user.dto'
+import { User } from 'src/user/entities/user.entity'
+
+export class CreateDspDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty({
+    message: 'E-Bind Number should not be empty',
+  })
+  @IsPhoneNumber('PH', {
+    message: 'E-Bind Number invalid',
+  })
+  e_bind_number: string
+
+  @ExistsInDb(MapId, 'area_id', {
+    message: 'Area ID does not exist',
+  })
+  @IsNotEmpty()
+  @IsNumber({}, { message: 'Invalid Area ID format' })
+  area_id: MapId
+
+  @IsString()
+  @NoDuplicateInDb(Dsp, 'dsp_code', {
+    message: 'DSP Code already exists',
+  })
+  dsp_code: string
+
+  @ExistsInDb(User, 'id', {
+    message: 'User ID does not exist',
+  })
+  @IsNotEmpty()
+  @IsUUID()
+  user: User
+}
--- src/dsp/entities/dsp.entity.ts
+++ src/dsp/entities/dsp.entity.ts
@@ -1,1 +1,37 @@
-export class Dsp {}
+import { MapId } from 'src/map-ids/entities/map-id.entity'
+import { User } from 'src/user/entities/user.entity'
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+@Entity()
+export class Dsp {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({})
+  dsp_code: string
+
+  @OneToOne(() => User, (user) => user.dsp, {
+    // cascade: true,
+    createForeignKeyConstraints: false,
+  })
+  @JoinColumn()
+  user: User
+
+  @JoinColumn()
+  @ManyToOne(() => MapId, (mapid) => mapid.dsp, {})
+  area_id: MapId
+
+  @Column({
+    nullable: true,
+  })
+  e_bind_number: string
+}

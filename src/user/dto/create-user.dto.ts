d08bcccda28ac713072a2d7571779609880cc425
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-12 17:24:39 +0800

    User DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
+import { PartialType } from '@nestjs/mapped-types'
 import {
+  IsEmail,
   IsNotEmpty,
-  IsNumberString,
   IsPhoneNumber,
   MaxLength,
+  MinLength,
 } from 'class-validator'
+import { Dsp } from 'src/dsp/entities/dsp.entity'
+import { ExistsInDb } from 'src/dsp/pipes/validation/ExistsInDb'
+import { NoDuplicateInDb } from 'src/dsp/pipes/validation/NoDuplicateInDb'
+import { User } from 'src/user/entities/user.entity'
 
 export class CreateUserDto {
   @IsNotEmpty({
@@ -23,4 +29,36 @@ export class CreateUserDto {
   })
   @IsPhoneNumber('PH', { message: 'Invalid Phone Number' })
   phone_number: string
+
+  @IsEmail(
+    {},
+    {
+      message: `Email format invalid`,
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email cannot be empty',
+  })
+  @NoDuplicateInDb(User, null, {
+    message: 'Email Already exists',
+  })
+  email: string
+
+  @IsNotEmpty({
+    message: `Password cannot be empty`,
+  })
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters',
+  })
+  password: string
+
+  @NoDuplicateInDb(User, null, {
+    message: 'Username already exists',
+  })
+  username?: string
+
+  @ExistsInDb(Dsp, null, {
+    message: `DSP Account doesn't exist`,
+  })
+  dsp?: Dsp
 }

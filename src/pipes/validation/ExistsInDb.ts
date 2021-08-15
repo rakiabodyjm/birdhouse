a70d58d58991aa4cd5a7471b3fda552fcdbcdfc3
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-15 21:19:51 +0800

    Create custom validation decorators

---
+++ src/pipes/validation/ExistsInDb.ts
@@ -0,0 +1,50 @@
+import { ClassConstructor } from 'class-transformer'
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator'
+import { getConnectionManager } from 'typeorm'
+
+export function ExistsInDb(
+  property: ClassConstructor<any>,
+  /**
+   * Field from entity
+   */
+  field: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: { [x: string]: any }, propertyName: string) {
+    registerDecorator({
+      name: 'existsInDb',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      // options: { ...validationOptions },
+      options: validationOptions,
+      validator: {
+        async validate(value: any, args: ValidationArguments) {
+          const entityRepositoryFindByField = await getConnectionManager()
+            .get('default')
+            .getRepository(property)
+            .findOne(value)
+            .catch((err) => {
+              console.log(err)
+              return false
+            })
+          console.log('Exists in DB Invoked', entityRepositoryFindByField)
+          console.log('Exists in DB args', args)
+          // console.log(
+          //   'entityRepositoryFindByField',
+          //   entityRepositoryFindByField,
+          // )
+          if (!entityRepositoryFindByField) {
+            return false
+          }
+          // console.log('args', args)
+          return true
+        },
+      },
+    })
+  }
+}
---
+++ src/pipes/validation/NoDuplicateInDb.ts
@@ -0,0 +1,50 @@
+import { ClassConstructor, classToPlain } from 'class-transformer'
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator'
+import { getConnectionManager } from 'typeorm'
+
+@ValidatorConstraint({ async: false })
+export class NoDuplicateInDbConstraints
+  implements ValidatorConstraintInterface
+{
+  async validate(incomingValue: string, args: ValidationArguments) {
+    args.constraints as [ClassConstructor<any>, string | string[]]
+    const [Class, columnName] = args.constraints
+    const searchForDuplicate = await getConnectionManager()
+      .get('default')
+      .getRepository(Class)
+      .findOne({
+        [columnName || args.property]: incomingValue,
+      })
+      .catch((err) => {
+        console.error('Error in NoDuplicateInDbP', err)
+        return null
+      })
+    if (searchForDuplicate) {
+      return false
+    }
+    return true
+  }
+}
+
+export function NoDuplicateInDb(
+  Class: ClassConstructor<any>,
+  columnName?: string | never,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: { [x: string]: any }, propertyName: string) {
+    return registerDecorator({
+      name: 'noDuplicateInDb',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [Class, columnName],
+      validator: NoDuplicateInDbConstraints,
+    })
+  }
+}

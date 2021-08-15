358161a7a87b71ef89064837ddabf711585270d7
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-15 21:11:46 +0800

    Initialize admin controller and service

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAdminDto } from './dto/create-admin.dto';
-import { UpdateAdminDto } from './dto/update-admin.dto';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Admin } from 'src/admin/entities/admin.entity'
+import { Repository } from 'typeorm'
+import { CreateAdminDto } from './dto/create-admin.dto'
+import { UpdateAdminDto } from './dto/update-admin.dto'
 
 @Injectable()
 export class AdminService {
-  create(createAdminDto: CreateAdminDto) {
-    return 'This action adds a new admin';
+  constructor(
+    @InjectRepository(Admin)
+    private readonly adminRepository: Repository<Admin>,
+  ) {}
+  async create(createAdminDto: CreateAdminDto) {
+    const newAdmin = this.adminRepository.create(createAdminDto)
+    await this.adminRepository.save(newAdmin)
+
+    return newAdmin
+    // return 'This action adds a new admin'
   }
 
-  findAll() {
-    return `This action returns all admin`;
+  async findAll() {
+    const admins = await this.adminRepository.find()
+    return admins
+    // return `This action returns all admin`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} admin`;
+    return `This action returns a #${id} admin`
   }
 
   update(id: number, updateAdminDto: UpdateAdminDto) {
-    return `This action updates a #${id} admin`;
+    return `This action updates a #${id} admin`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} admin`;
+    return `This action removes a #${id} admin`
   }
 }

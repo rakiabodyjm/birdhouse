0af8eeb1d14be21a871e49be41a55d764011b0d8
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-15 21:12:15 +0800

    Add CreateAdminDTO

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAdminDto {}
+import { IsString } from 'class-validator'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+import { User } from 'src/user/entities/user.entity'
+
+export class CreateAdminDto {
+  @IsString()
+  @ExistsInDb(User, 'id')
+  user: User
+}

11b20a51da396b413f2e1aa7ced67e5316f7b8f2
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-15 21:11:18 +0800

    Add Swagger into main module

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'
+    return 'REALM1000 | DITO DATABASE v1.0'
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { useContainer } from 'typeorm'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  let app = await NestFactory.create(AppModule, {
-    logger: true,
+  let app = await NestFactory.create(AppModule)
+
+  /**
+   * Swagger Configuration
+   */
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Realm1000 DITO API')
+    .setDescription('The REALM1000 | DITO Database API Documentation. ')
+    .setVersion('1.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
   })
-  app = app.useGlobalPipes(new ValidationPipe())
+
+  /**
+   * Global validation pipe
+   */
+  app = app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  )
   const port = process.env.PORT || 6000
 
   await app.listen(port)

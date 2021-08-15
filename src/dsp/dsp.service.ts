c8804a8d590189a457e1ec2ddb652b4fa5b732ba
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-15 21:19:18 +0800

    Remove unneeded import

--- src/dsp/dsp.service.ts
+++ src/dsp/dsp.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Dsp } from 'src/dsp/entities/dsp.entity'
 import { Repository } from 'typeorm'

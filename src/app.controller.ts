7dc05bfc0217adca60588d60b35e40ea31e8bd01
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-08-24 12:50:19 +0800

    Comment default helloWorld from swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,8 +7,8 @@ import { Request } from 'express'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello()
+  // }
 }

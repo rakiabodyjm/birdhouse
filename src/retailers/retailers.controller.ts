0cd3c9928fa95316e468ec8c2469938416e67551
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-12 21:54:27 +0800

    Retailer Module controller

--- src/retailers/retailers.controller.ts
+++ src/retailers/retailers.controller.ts
@@ -6,28 +6,38 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common'
 import { RetailersService } from './retailers.service'
 import { CreateRetailerDto } from './dto/create-retailer.dto'
 import { UpdateRetailerDto } from './dto/update-retailer.dto'
+import { Retailer } from 'src/retailers/entities/retailer.entity'
+import { Paginated } from 'src/types/Paginated'
+import { createEntityMessage } from 'src/types/EntityMessage'
 
 @Controller('retailers')
+@UseInterceptors(ClassSerializerInterceptor)
 export class RetailersController {
   constructor(private readonly retailersService: RetailersService) {}
 
   @Post()
-  create(@Body() createRetailerDto: CreateRetailerDto) {
+  create(@Body() createRetailerDto: CreateRetailerDto): Promise<Retailer> {
     return this.retailersService.create(createRetailerDto)
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Paginated<Retailer> | Retailer[]> {
     return this.retailersService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.retailersService.findOne(+id)
+    return this.retailersService.findOne(id).catch((err) => {
+      throw new HttpException(err.message, HttpStatus.NOT_FOUND)
+    })
   }
 
   @Patch(':id')
@@ -35,11 +45,16 @@ export class RetailersController {
     @Param('id') id: string,
     @Body() updateRetailerDto: UpdateRetailerDto,
   ) {
-    return this.retailersService.update(+id, updateRetailerDto)
+    return this.retailersService.update(id, updateRetailerDto).catch((err) => {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST)
+    })
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.retailersService.remove(+id)
+  async remove(@Param('id') id: string) {
+    const retailer = await this.retailersService.remove(id).catch((err) => {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST)
+    })
+    return createEntityMessage(retailer, 'Entity Retailer Deleted')
   }
 }

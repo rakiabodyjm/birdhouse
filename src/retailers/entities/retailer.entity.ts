5914720659c4800cf9ed7882f3d2e5db8936ae22
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-09-13 21:55:33 +0800

    Retailer Entity

--- src/retailers/entities/retailer.entity.ts
+++ src/retailers/entities/retailer.entity.ts
@@ -1,15 +1,45 @@
+import { Expose } from 'class-transformer'
 import { Dsp } from 'src/dsp/entities/dsp.entity'
 import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { User } from 'src/user/entities/user.entity'
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
 
 @Entity()
 export class Retailer {
   @PrimaryGeneratedColumn('uuid')
   id: string
-
+  @Expose()
   @ManyToOne((type) => Subdistributor, (subd) => subd.retailers)
   subdistributor: Subdistributor
 
+  @Expose()
   @ManyToOne((type) => Dsp, (dsp) => dsp.retailers)
   dsp: Dsp
+
+  @Expose()
+  @OneToOne((type) => User, (user) => user.retailer, {
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn()
+  user: User
+
+  @Column()
+  e_bind_number: string
+
+  @Column({ default: `` })
+  store_name: string
+
+  @Column()
+  id_type: string
+
+  @Column()
+  id_number: string
 }

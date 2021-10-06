28eb8920ae5fa15955bc6241a2a490080da89c45
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-06 16:35:15 +0800

    Add login DTO to Swagger ApiProperty

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,16 +1,18 @@
-import { Optional } from '@nestjs/common'
-import { PartialType } from '@nestjs/mapped-types'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   email: string
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   password: string
 
+  @ApiProperty()
   @IsBoolean()
   remember_me: boolean
 }

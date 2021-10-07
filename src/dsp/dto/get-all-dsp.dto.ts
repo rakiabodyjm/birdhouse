a8e87702904c92b14f5132e7716eaecb03eaff50
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-10-07 16:39:19 +0800

    Add countOnly option in getAllDspDTO and Subdistributor option

--- src/dsp/dto/get-all-dsp.dto.ts
+++ src/dsp/dto/get-all-dsp.dto.ts
@@ -1,4 +1,37 @@
+import { HttpException, HttpStatus } from '@nestjs/common'
 import { PartialType } from '@nestjs/swagger'
+import { Transform, Type } from 'class-transformer'
+import { IsBoolean, IsBooleanString, IsOptional, IsUUID } from 'class-validator'
+import { CreateDspDto } from 'src/dsp/dto/create-dsp.dto'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
 import { PaginateOptions } from 'src/types/Paginated'
 
-export class GetAllDspDto extends PartialType(PaginateOptions) {}
+export class GetAllDspDto extends PartialType(PaginateOptions) {
+  @IsOptional()
+  @IsUUID()
+  @ExistsInDb(Subdistributor, 'id', {
+    message: `Subdistributor for this DSP doesn't exist`,
+  })
+  subdistributor: string
+
+  /**
+   * note order is important
+   */
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (!['true', 'false'].includes(value)) {
+      throw new HttpException(
+        `Invalid boolean value for countOnly`,
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
+    if (value === 'true') {
+      return true
+    } else {
+      return false
+    }
+  })
+  countOnly: boolean
+}

2c9054a45ae4997aa0905cb652100945d7aff410
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-10 22:33:46 +0800

    dsp search with filter

--- src/dsp/dto/search-dsp.dto.ts
+++ src/dsp/dto/search-dsp.dto.ts
@@ -1,6 +1,13 @@
-import { IsNotEmpty } from 'class-validator'
+import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
 
 export class SearchDspDto {
   @IsNotEmpty()
   searchQuery: string
+
+  @IsOptional()
+  @IsUUID()
+  @ExistsInDb(Subdistributor, 'id')
+  subdistributor?: string
 }

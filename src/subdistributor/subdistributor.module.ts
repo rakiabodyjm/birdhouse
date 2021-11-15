87cea5ebf2edb6f7a3ec158f7d17d8bf13612040
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-15 17:54:24 +0800

    Updated rest files

--- src/subdistributor/subdistributor.module.ts
+++ src/subdistributor/subdistributor.module.ts
@@ -3,9 +3,10 @@ import { SubdistributorService } from './subdistributor.service'
 import { SubdistributorController } from './subdistributor.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
+import { CeasarModule } from 'src/ceasar/ceasar.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subdistributor])],
+  imports: [TypeOrmModule.forFeature([Subdistributor]), CeasarModule],
   controllers: [SubdistributorController],
   providers: [SubdistributorService],
   exports: [SubdistributorService],

c89c01f3507f6890b6b9155922d0d1988d9ffccc
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-21 15:29:26 +0800

    ExternalCeasar initialize typings

--- src/external-ceasar/external-ceasar.service.ts
+++ src/external-ceasar/external-ceasar.service.ts
@@ -9,6 +9,7 @@ export class ExternalCeasarService {
     @InjectRepository(ExternalCeasar)
     private readonly externalCeasarRepo: Repository<ExternalCeasar>,
   ) {}
+
   create(newCeasar: Partial<ExternalCeasar>) {
     return this.externalCeasarRepo
       .save(this.externalCeasarRepo.create(newCeasar))
@@ -38,5 +39,107 @@ export class ExternalCeasarService {
       .catch((err) => {
         throw new Error(err)
       })
+  }
+}
+
+class InitialParams {
+  api_key: string
+  merchant_url: string
+}
+
+interface CreateWalletParams extends InitialParams {
+  action: 'createWallet'
+  lastname: string
+  firstname: string
+  middlename: string
+  contact_no: string
+  email_address: string
+  password: string
+  user_type:
+    | 'User'
+    | 'Retailer'
+    | 'DSP'
+    | 'Sub-Distributor'
+    | 'Distributor'
+    | 'Master Distributor'
+}
+
+interface CreateWalletResponse {
+  data: {
+    walletId: string
+  }
+}
+
+interface GetWalletNameParams extends InitialParams {
+  action: 'getWallet'
+  wallet_id: string
+}
+interface GetWalletResponse {
+  data: {
+    walletName: string
+  }
+}
+
+interface GetWalletBalanceParams extends InitialParams {
+  action: 'getWalletBalance'
+  wallet_id: string
+}
+
+interface GetWalletBalanceResponse {
+  data: {
+    balance: number
+  }
+}
+
+interface GetUserInfoParams extends InitialParams {
+  action: 'getUserInfo'
+  wallet_id: string
+}
+
+interface GetUserInfoResponse {
+  data: {
+    lastName: string
+    firstName: string
+    middleName: string
+    address: string | null
+    contactNo: string
+    emailAddress: string
+    country: string
+  }
+}
+
+interface GetMerchantWalletId extends InitialParams {
+  action: 'getMerchantWalletId'
+  username: string
+  password: string
+}
+
+interface GetMerchantWalletIdResponse {
+  walletId: string
+}
+
+interface GetCCoinWalletEquivalentToPesoAndUsd extends InitialParams {
+  action: 'getEquivalent'
+  wallet_id: string
+}
+
+interface PayUsingCCoinWallet extends InitialParams {
+  action: 'payUsingCaesar'
+  customer_id: string
+  amount: number
+  currency: 'PHP' | 'USD'
+  description: string
+  merchant_id: string
+}
+
+interface PayUsingCCoinWalletResponse {
+  data: {
+    status: 'payment_successful'
+    referenceNumber: string
+    description: string
+    amountPaid: number
+    currency: 'PHP' | 'USD'
+    amountPaidInCCoin: number
+    balance: number
   }
 }

0407f8eebfafaca33becaf31b9d743e9b75908c2
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-22 15:30:44 +0800

    Import assetModule into inventoryModule

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -4,9 +4,10 @@ import { InventoryController } from './inventory.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import Inventory from 'src/inventory/entities/inventory.entity'
 import { CeasarModule } from 'src/ceasar/ceasar.module'
+import { AssetModule } from 'src/asset/asset.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inventory]), CeasarModule],
+  imports: [TypeOrmModule.forFeature([Inventory]), CeasarModule, AssetModule],
   controllers: [InventoryController],
   providers: [InventoryService],
   exports: [InventoryService],

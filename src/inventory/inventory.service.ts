77fa8cd8a4f7d6be5bdb88bcf5b9f19ce080381b
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-11-22 15:31:35 +0800

    Add inventoryService method findByAssetIdAndCeasarId and fixes to update method

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { isNotEmptyObject } from 'class-validator'
+import Asset from 'src/asset/entities/asset.entity'
+import { Ceasar } from 'src/ceasar/entities/ceasar.entity'
+import { GetAllInventoryDto } from 'src/inventory/dto/get-all-inventory.dto'
 import Inventory from 'src/inventory/entities/inventory.entity'
+import paginateFind from 'src/utils/paginate'
 import { Repository } from 'typeorm'
 import { CreateInventoryDto } from './dto/create-inventory.dto'
 import { UpdateInventoryDto } from './dto/update-inventory.dto'
@@ -14,16 +19,49 @@ export class InventoryService {
     this.relations = ['ceasar', 'asset']
   }
   relations: string[]
-  create(createInventoryDto: CreateInventoryDto) {
+
+  create(createInventoryDto: {
+    quantity: number
+    asset: Asset
+    ceasar: Ceasar
+  }) {
     const newInventory = this.inventoryRepository.create(createInventoryDto)
+
     return this.inventoryRepository.save(newInventory)
   }
 
-  findAll() {
+  findAll(paginationParams?: GetAllInventoryDto) {
+    if (isNotEmptyObject(paginationParams)) {
+      return paginateFind(this.inventoryRepository, paginationParams, {
+        relations: this.relations,
+      })
+    }
     return this.inventoryRepository.find({
       relations: this.relations,
     })
+
     // return `This action returns all inventory`
+  }
+
+  findByAssetIdAndCeasarId({
+    asset_id,
+    ceasar_id,
+  }: {
+    asset_id: string
+    ceasar_id: string
+  }) {
+    return this.inventoryRepository
+      .findOne({
+        asset: {
+          id: asset_id,
+        },
+        ceasar: {
+          id: ceasar_id,
+        },
+      })
+      .catch((err) => {
+        throw new Error(err.message)
+      })
   }
 
   findOne(id: string) {
@@ -38,13 +76,7 @@ export class InventoryService {
   }
 
   update(id: string, updateInventoryDto: UpdateInventoryDto) {
-    return this.inventoryRepository
-      .findOneOrFail(
-        { id },
-        {
-          relations: this.relations,
-        },
-      )
+    return this.findOne(id)
       .then((inventory) => {
         return this.inventoryRepository.save({
           ...inventory,

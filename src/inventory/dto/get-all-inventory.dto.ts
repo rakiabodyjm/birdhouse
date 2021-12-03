ac9c717a68601058eb592cab8e02ff7980fb7c7f
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-03 16:20:59 +0800

    Update GetAllInventoryDto to InventoryController and InventoryService

--- src/inventory/dto/get-all-inventory.dto.ts
+++ src/inventory/dto/get-all-inventory.dto.ts
@@ -1,8 +1,63 @@
+import { BadRequestException } from '@nestjs/common'
 import { PartialType } from '@nestjs/mapped-types'
-import { IsBooleanString } from 'class-validator'
+import { Transform } from 'class-transformer'
+import { IsOptional, IsUUID } from 'class-validator'
+import { Admin } from 'src/admin/entities/admin.entity'
+import { Dsp } from 'src/dsp/entities/dsp.entity'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+import { Retailer } from 'src/retailers/entities/retailer.entity'
+import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
+import { User } from 'src/user/entities/user.entity'
 import { PaginateOptions } from '../../types/Paginated'
 
 export class GetAllInventoryDto extends PartialType(PaginateOptions) {
-  @IsBooleanString()
+  @Transform(({ value }) => {
+    try {
+      if (value) {
+        if (value === 'true') {
+          return true
+        } else if (value === 'false') {
+          return false
+        } else {
+          throw new Error(
+            `active parameter must only be of values true or false`,
+          )
+        }
+      } else {
+        throw new Error(
+          `Value must be either true, false or not defined in parameters`,
+        )
+      }
+    } catch (err) {
+      console.error('getAllInventory error', err)
+      throw new BadRequestException(err.message)
+    }
+  })
+  @IsOptional()
   disabled?: true
+
+  @IsOptional()
+  @ExistsInDb(User, 'id')
+  @IsUUID()
+  user?: string
+
+  @IsOptional()
+  @ExistsInDb(Admin, 'id')
+  @IsUUID()
+  admin?: string
+
+  @IsOptional()
+  @ExistsInDb(Subdistributor, 'id')
+  @IsUUID()
+  subdistributor?: string
+
+  @IsOptional()
+  @ExistsInDb(Dsp, 'id')
+  @IsUUID()
+  dsp?: string
+
+  @IsOptional()
+  @ExistsInDb(Retailer, 'id')
+  @IsUUID()
+  retailer?: string
 }

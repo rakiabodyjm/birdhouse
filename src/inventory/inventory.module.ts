7d93791902a36979f6ffef4f7afc04ff32e8524d
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-04 16:21:57 +0800

    Import Auth and UserModule, initialize InventoryLog Entity, initialize middleware

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -1,15 +1,38 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { InventoryService } from './inventory.service'
 import { InventoryController } from './inventory.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import Inventory from 'src/inventory/entities/inventory.entity'
 import { CeasarModule } from 'src/ceasar/ceasar.module'
 import { AssetModule } from 'src/asset/asset.module'
+import { AuthModule } from 'src/auth/auth.module'
+import { UserModule } from 'src/user/user.module'
+import { InventoryLog } from 'src/inventory/entities/inventory-logs.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inventory]), CeasarModule, AssetModule],
+  imports: [
+    TypeOrmModule.forFeature([Inventory, InventoryLog]),
+    CeasarModule,
+    AssetModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [InventoryController],
   providers: [InventoryService],
   exports: [InventoryService],
 })
-export class InventoryModule {}
+export class InventoryModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //Use InventoryCrudLoggerMiddleware
+    // consumer
+    //   .apply(InventoryCrudLoggerMiddleware)
+    //   .exclude({
+    //     path: 'inventory',
+    //     method: RequestMethod.GET,
+    //   })
+    //   .forRoutes(InventoryController)
+    /**
+     * we can insert another consumer.apply for another middleware
+     */
+  }
+}

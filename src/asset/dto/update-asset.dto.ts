81b3df48b0d032280f32590dc871f63e416d393f
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-21 16:13:21 +0800

    Update asset controller to accommodate asset approval

--- src/asset/dto/update-asset.dto.ts
+++ src/asset/dto/update-asset.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/swagger'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
+import { Transform } from 'class-transformer'
+import { IsArray, IsOptional } from 'class-validator'
 import { CreateAssetDto } from './create-asset.dto'
 
-export class UpdateAssetDto extends PartialType(CreateAssetDto) {}
+export class UpdateAssetDto extends PartialType(CreateAssetDto) {
+  @ApiProperty({
+    type: 'string[]',
+  })
+  @IsOptional()
+  @Transform(({ value }) => {
+    return Array.isArray(value)
+      ? Array.length > 0
+        ? JSON.stringify(value)
+        : null
+      : value
+  })
+  approval: string[]
+}

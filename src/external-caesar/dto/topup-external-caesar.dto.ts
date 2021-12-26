43ae4a907d4f7f1edc433fd1b96ff198017de907
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-26 16:20:08 +0800

    CaesarWallet API simulation

---
+++ src/external-caesar/dto/topup-external-caesar.dto.ts
@@ -0,0 +1,20 @@
+import { Transform } from 'class-transformer'
+import { IsNotEmpty, IsNumber, IsNumberString } from 'class-validator'
+import { ExternalCaesar } from 'src/external-caesar/entities/external-caesar.entity'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+
+export class TopUpExternalCaesarBodyDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @Transform(({ value }) => {
+    return Number(value)
+  })
+  amount: number
+}
+export class TopUpExternalCaesarParamDto {
+  @IsNotEmpty()
+  @ExistsInDb(ExternalCaesar, 'wallet_id', {
+    message: `Caesar wallet_id doesn't exist`,
+  })
+  id: string
+}

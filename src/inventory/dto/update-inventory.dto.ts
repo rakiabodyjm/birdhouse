2fa7eb26b27a502eb96f145ce4bead92d27ea73e
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-28 16:22:07 +0800

    Updates to inventory management admin procuring inventory

--- src/inventory/dto/update-inventory.dto.ts
+++ src/inventory/dto/update-inventory.dto.ts
@@ -1,9 +1,51 @@
-import { PartialType } from '@nestjs/swagger'
-import { IsNotEmpty, IsNumber } from 'class-validator'
-import { CreateInventoryDto } from './create-inventory.dto'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsNumber, IsOptional, Min } from 'class-validator'
 
 export class UpdateInventoryDto {
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
+  @IsOptional()
+  @IsOptional()
   quantity: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  name: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  description: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @Min(0.01)
+  unit_price: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @Min(0.01)
+  srp_for_subd: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @Min(0.01)
+  srp_for_dsp: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @Min(0.01)
+  srp_for_retailer: number
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsOptional()
+  @Min(0.01)
+  srp_for_user: number
 }

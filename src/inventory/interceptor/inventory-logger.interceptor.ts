6e828153d1a1baf3ee72af0723998fb30741b2f4
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-29 16:23:22 +0800

    InventoryLogger updates module

--- src/inventory/interceptor/inventory-logger.interceptor.ts
+++ src/inventory/interceptor/inventory-logger.interceptor.ts
@@ -3,11 +3,16 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
+  RequestMethod,
 } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Observable, tap } from 'rxjs'
+import { Request } from 'express'
+import { catchError, map, Observable } from 'rxjs'
+import { ROLES_KEY } from 'src/auth/decorators/roles.decorator'
 import { CaesarService } from 'src/caesar/caesar.service'
 import { InventoryLog } from 'src/inventory/entities/inventory-logs.entity'
+import { Roles } from 'src/types/Roles'
 import { Repository } from 'typeorm'
 
 @Injectable()
@@ -15,21 +20,52 @@ export class InventoryLoggerInterceptor implements NestInterceptor {
   constructor(
     private caesarService: CaesarService,
     @InjectRepository(InventoryLog)
-    inventoryLogsRepository: Repository<InventoryLog>,
+    private inventoryLogsRepository: Repository<InventoryLog>,
+    private reflector: Reflector,
   ) {}
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // console.log('Before ...')
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
+    const req: Request = context.switchToHttp().getRequest()
+    if (['POST', 'PATCH', 'DELETE'].includes(req.method)) {
+      const role = this.reflector.getAllAndOverride<Roles[]>(ROLES_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ])
 
-    const now = Date.now()
+      // const now = Date.now()
+      /**
+       * Get user and method variables from request
+       */
+      const { user, method } = req
+      const caesar = await this.caesarService.findOne({
+        [role[0]]: user[`${role[0]}`],
+      })
 
-    const req = context.switchToHttp().getRequest()
-    if (['GET', 'POST', 'PATCH', 'DELETE'].includes(req.method)) {
-      // console.log('HEADERS', req.headers)
+      const inventoryLog = this.inventoryLogsRepository.create({
+        created_at: Date.now(),
+        data: JSON.stringify(req?.body),
+        method: method as keyof typeof RequestMethod,
+        caesar: caesar,
+      })
+      next.handle().subscribe({
+        error: async (err) => {
+          inventoryLog.remarks = err.message
+          inventoryLog.created_at = new Date()
+          await this.inventoryLogsRepository.save(inventoryLog)
+        },
+        complete: async () => {
+          inventoryLog.remarks = JSON.stringify('SUCCESS')
+          inventoryLog.created_at = new Date()
+          await this.inventoryLogsRepository.save(inventoryLog)
+        },
+      })
+
+      return next.handle()
     }
-    // console.log(req.user)
 
-    return next
-      .handle()
-      .pipe(tap(() => console.log(`After ... ${Date.now() - now}ms`)))
+    return next.handle()
+    // .pipe(tap(() => console.log(`After ... ${Date.now() - now}ms`)))
   }
 }

cb57262c54ab2f3126f23552f48c41ef18532418
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2021-12-29 16:23:06 +0800

    Updates to inventory management admin procuring inventory

--- src/inventory/entities/inventory.entity.ts
+++ src/inventory/entities/inventory.entity.ts
@@ -1,14 +1,22 @@
 import Asset from 'src/asset/entities/asset.entity'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
+import { PendingTransaction } from 'src/transaction/entities/pending-transaction.entity'
+import { Transaction } from 'src/transaction/entities/transaction.entity'
 import {
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
 } from 'typeorm'
 
 @Entity()
+@Unique(['asset', 'caesar'])
 export default class Inventory {
   @PrimaryGeneratedColumn('uuid')
   id: string
@@ -25,9 +33,89 @@ export default class Inventory {
   @JoinColumn({
     name: 'caesar_id',
   })
-  @ManyToOne((type) => Caesar, (caesar) => caesar.inventory)
+  @ManyToOne((type) => Caesar, (caesar) => caesar.inventory, {
+    createForeignKeyConstraints: false,
+    nullable: false,
+  })
   caesar: Caesar
 
-  @Column()
+  @Column({
+    default: '',
+  })
+  name: string
+
+  @Column({
+    nullable: true,
+  })
+  description: string
+
+  @Column('decimal', {
+    precision: 18,
+    scale: 4,
+  })
+  unit_price: number
+
+  @Column('decimal', {
+    precision: 18,
+    scale: 4,
+  })
+  srp_for_subd: number
+
+  @Column('decimal', {
+    precision: 18,
+    scale: 4,
+  })
+  srp_for_dsp: number
+
+  @Column('decimal', {
+    precision: 18,
+    scale: 4,
+  })
+  srp_for_retailer: number
+
+  @Column('decimal', {
+    precision: 18,
+    scale: 4,
+  })
+  srp_for_user: number
+
+  @OneToMany(() => Transaction, (transaction) => transaction.inventory_from)
+  from_transactions?: Transaction[]
+
+  @OneToMany(() => Transaction, (transactions) => transactions.inventory_to)
+  to_transactions?: Transaction[]
+
+  @OneToMany(
+    () => PendingTransaction,
+    (pendingTranscations) => pendingTranscations.inventory,
+  )
+  pending_transactions: PendingTransaction[]
+
+  @Column({
+    default: true,
+  })
   active: boolean
+
+  /**
+   * JSON Data
+   */
+  @Column({
+    nullable: true,
+  })
+  restrictions: string
+
+  @CreateDateColumn({
+    type: 'datetime',
+  })
+  created_at: Date
+
+  @UpdateDateColumn({
+    type: 'datetime',
+  })
+  updated_at: Date
+
+  @DeleteDateColumn({
+    type: 'datetime',
+  })
+  deleted_at: Date
 }

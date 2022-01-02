f6acc94ce71bbe91c75cc046b7b48dd999b6d919
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-02 16:30:40 +0800

    Transaction and PendingTransaction module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -6,15 +6,27 @@ import { Transaction } from 'src/transaction/entities/transaction.entity'
 import { InventoryModule } from 'src/inventory/inventory.module'
 import { CaesarModule } from 'src/caesar/caesar.module'
 import { AssetModule } from 'src/asset/asset.module'
+import PendingTransactionController from 'src/transaction/pending-transaction.controller'
+import { PendingTransaction } from 'src/transaction/entities/pending-transaction.entity'
+import { PendingTransactionService } from 'src/transaction/pending-transaction.service'
+import { DspModule } from 'src/dsp/dsp.module'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Transaction]),
+    TypeOrmModule.forFeature([Transaction, PendingTransaction]),
     InventoryModule,
     CaesarModule,
     AssetModule,
+    DspModule,
   ],
-  controllers: [TransactionController],
-  providers: [TransactionService],
+  controllers: [TransactionController, PendingTransactionController],
+  providers: [PendingTransactionService, TransactionService],
+  exports: [TransactionService, PendingTransactionService],
 })
 export class TransactionModule {}
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([PendingTransaction]), TransactionModule],
+//   providers: [PendingTransactionService],
+// })
+// export class PendingTranactionModule {}

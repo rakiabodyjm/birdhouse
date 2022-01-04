43805cbeef935a4a72c695a56388010a6d41fefe
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-04 16:36:51 +0800

    PurchaseTransactionDto

--- src/transaction/dto/purchase-transaction.dto.ts
+++ src/transaction/dto/purchase-transaction.dto.ts
@@ -1,19 +1,27 @@
+import { PartialType } from '@nestjs/swagger'
 import { IsNotEmpty, IsNumber, IsUUID } from 'class-validator'
+import { Caesar } from 'src/caesar/entities/caesar.entity'
 import Inventory from 'src/inventory/entities/inventory.entity'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 
-export class PurchaseFromInventoryDto {
+export class PurchaseTransactionBodyDto {
+  @IsNumber()
+  @IsNotEmpty()
+  quantity: number
+}
+
+export class PurchaseTransactionParamsDto {
   @IsNotEmpty()
   @ExistsInDb(Inventory, 'id', {
     message: `Inventory doesn't exist`,
   })
-  id: string
-}
-export class PurchaseTransactionDto {
-  @IsNotEmpty()
-  @IsNumber()
-  quantity: number
+  // @IsNumber()
+  inventory_from: string
 
+  @IsNotEmpty()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar doesn't exist for this buyer`,
+  })
   @IsUUID()
-  seller: string
+  buyer: string
 }

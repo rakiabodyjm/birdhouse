4cc1d15295bfbedb1867b8a7c2d1fef62d99ec50
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-05 16:37:07 +0800

    PendingTransactionDto

---
+++ src/transaction/entities/pending-transaction.entity.ts
@@ -0,0 +1,87 @@
+import { Expose } from 'class-transformer'
+import { Caesar } from 'src/caesar/entities/caesar.entity'
+import Inventory from 'src/inventory/entities/inventory.entity'
+import { Transaction } from 'src/transaction/entities/transaction.entity'
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class PendingTransaction {
+  @PrimaryGeneratedColumn('increment')
+  id: string
+
+  @Column()
+  pending_purchase_id: string
+
+  @Column({
+    default: false,
+  })
+  approved: boolean
+
+  @ManyToOne((type) => Inventory, (inventory) => inventory.pending_transactions)
+  @JoinColumn({
+    name: 'inventory_to_be_bought',
+  })
+  inventory: Inventory
+
+  @ManyToOne(
+    (type) => Caesar,
+    (caesar) => caesar.pending_transactions_as_buyer,
+    {
+      eager: true,
+    },
+  )
+  @JoinColumn({
+    name: 'buyer_caesar',
+  })
+  caesar_buyer: Caesar
+
+  @ManyToOne(
+    (type) => Caesar,
+    (ceasar) => ceasar.pending_transactions_as_seller,
+    {
+      eager: true,
+    },
+  )
+  @JoinColumn({
+    name: 'caesar_seller',
+  })
+  caesar_seller: Caesar
+
+  @Column()
+  quantity: number
+
+  @JoinColumn({
+    name: 'transaction_id',
+  })
+  @ManyToOne(
+    () => Transaction,
+    (transaction) => transaction.pending_transaction,
+  )
+  transaction_id?: Transaction
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at: Date
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date
+
+  @DeleteDateColumn({
+    type: 'datetime',
+  })
+  deleted_at: Date
+}

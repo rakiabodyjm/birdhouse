0b57e54d70ce99bf9888bb62106ab9d2c4440232
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-08 16:40:38 +0800

    Fixes to paginate function

--- src/utils/paginate.ts
+++ src/utils/paginate.ts
@@ -6,19 +6,22 @@ export default async function paginateFind<T>(
   options: PaginateOptions,
   findOptions?: FindManyOptions<T>,
 ): Promise<Paginated<T>> {
-  const [, total] = await repository.findAndCount({
-    ...findOptions,
-  })
   const page = options?.page || 0
   const limit = options?.limit || 100
 
-  const total_page = Math.ceil(total / limit)
-
-  const data = await repository.find({
+  const [data, total] = await repository.findAndCount({
     ...findOptions,
     take: limit,
     skip: page * limit,
   })
+
+  const total_page = Math.ceil(total / limit)
+
+  // const data = await repository.find({
+  //   ...findOptions,
+  //   take: limit,
+  //   skip: page * limit,
+  // })
 
   return {
     data,

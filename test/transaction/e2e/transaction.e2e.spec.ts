e9b5f0ca04645106aacb33c0884902f0a355f477
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-09 16:41:24 +0800

    Initialize e2e test on transaction

---
+++ test/transaction/e2e/transaction.e2e.spec.ts
@@ -0,0 +1,37 @@
+import * as request from 'supertest'
+import { Test } from '@nestjs/testing'
+
+import { INestApplication } from '@nestjs/common'
+import { TransactionModule } from 'src/transaction/transaction.module'
+import { TransactionService } from 'src/transaction/transaction.service'
+
+describe('Cats', () => {
+  let app: INestApplication
+  const catsService = { findAll: () => ['test'] }
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TransactionModule],
+    })
+      //   .overrideProvider(TransactionService)
+      //   .useValue(catsService)
+      .compile()
+
+    app = moduleRef.createNestApplication()
+    await app.init()
+  })
+
+  it('/GET transaction', () => {
+    return request(app.getHttpServer()).get('/').expect(200)
+  })
+
+  //   it(`/GET cats`, () => {
+  //     return request(app.getHttpServer()).get('/cats').expect(200).expect({
+  //       data: catsService.findAll(),
+  //     })
+  //   })
+
+  afterAll(async () => {
+    await app.close()
+  })
+})

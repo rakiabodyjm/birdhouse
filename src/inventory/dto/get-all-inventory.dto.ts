744aebdf6701f91c17bc322ce91d9ddaae6e6283
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-21 17:55:55 +0800

    GetAllInventory querying by asset

--- src/inventory/dto/get-all-inventory.dto.ts
+++ src/inventory/dto/get-all-inventory.dto.ts
@@ -1,15 +1,28 @@
 import { BadRequestException } from '@nestjs/common'
 import { PartialType } from '@nestjs/mapped-types'
 import { Transform } from 'class-transformer'
-import { IsOptional, IsUUID } from 'class-validator'
+import { IsIn, IsOptional, IsUUID } from 'class-validator'
 import { Admin } from 'src/admin/entities/admin.entity'
+import Asset from 'src/asset/entities/asset.entity'
+import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { Dsp } from 'src/dsp/entities/dsp.entity'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 import { Retailer } from 'src/retailers/entities/retailer.entity'
 import { Subdistributor } from 'src/subdistributor/entities/subdistributor.entity'
+import { SRPRoles } from 'src/types/Roles'
 import { User } from 'src/user/entities/user.entity'
 import { PaginateOptions } from '../../types/Paginated'
 
+// export class ForDto {
+//   // @IsOptional()
+//   @IsIn(Object.keys(SRPRoles))
+//   account_type?: string
+
+//   @ExistsInDb(Caesar, 'id', {
+//     message: `GetAllInventory for this caesar account doesn't exist `,
+//   })
+//   caesar: string
+// }
 export class GetAllInventoryDto extends PartialType(PaginateOptions) {
   @Transform(({ value }) => {
     try {
@@ -60,4 +73,8 @@ export class GetAllInventoryDto extends PartialType(PaginateOptions) {
   @ExistsInDb(Retailer, 'id')
   @IsUUID()
   retailer?: string
+
+  @IsOptional()
+  @ExistsInDb(Asset, 'id')
+  asset?: string
 }

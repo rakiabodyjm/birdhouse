3cba2244bc29dd73df3add5a6276bf041f976e7c
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-01-30 20:09:49 +0800

    Add account_type option in findAll CaesarService method to query certain account types caesar accounts

--- src/caesar/dto/get-all-caesar.dto.ts
+++ src/caesar/dto/get-all-caesar.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger'
+import { IsOptional } from 'class-validator'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 import { PaginateOptions } from 'src/types/Paginated'
+import { UserTypesAndUser } from 'src/types/Roles'
 
-export class GetAllCaesarDto extends PartialType(PaginateOptions) {}
+export class GetAllCaesarDto extends PartialType(PaginateOptions) {
+  @IsOptional()
+  account_type?: UserTypesAndUser
+}

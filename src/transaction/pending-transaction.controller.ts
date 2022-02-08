7931a02b64c6e2c9d5fd279eddcb4cd33de65020
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-02-08 14:44:51 +0800

    Fixes to Inventory Logger

--- src/transaction/pending-transaction.controller.ts
+++ src/transaction/pending-transaction.controller.ts
@@ -2,18 +2,22 @@ import {
   BadRequestException,
   ClassSerializerInterceptor,
   Controller,
+  Delete,
   Get,
   InternalServerErrorException,
   Param,
   Post,
   Query,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 import { ApiTags } from '@nestjs/swagger'
 import { ApprovePendingTransactionDto } from 'src/transaction/dto/approve-pending-transaction.dto'
+import { CancelPendingTransactionDto } from 'src/transaction/dto/cancel-pending-transaction.dto'
 import GetAllPendingTransactionDto from 'src/transaction/dto/get-all-pending-transaction.dto'
 import { PendingTransactionService } from 'src/transaction/pending-transaction.service'
-import { TransactionService } from 'src/transaction/transaction.service'
+import { createEntityMessage } from 'src/types/EntityMessage'
 
 @ApiTags('Pending Transaction Routes')
 @Controller('pending-transaction')
@@ -22,13 +26,19 @@ export default class PendingTransactionController {
   constructor(
     private readonly pendingTransactionService: PendingTransactionService,
   ) {}
+
+  @Get(':id')
+  findOne(@Param() id: string) {
+    return this.pendingTransactionService.findOne(id)
+  }
+
   @Get()
   findAll(@Query() getAllPendingTransactionDto: GetAllPendingTransactionDto) {
     console.log('getAllPendingTransactionDto', getAllPendingTransactionDto)
     return this.pendingTransactionService.findAll(getAllPendingTransactionDto)
   }
 
-  // @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'))
   @Post('approve-transaction/:pending_transaction_id')
   async approveTransaction(
     @Param('pending_transaction_id') id: ApprovePendingTransactionDto['id'],
@@ -36,14 +46,64 @@ export default class PendingTransactionController {
     try {
       const returner = await this.pendingTransactionService
         .approvePendingTransaction(id)
+        .then((response) => {
+          if (response?.transaction) {
+            return createEntityMessage(
+              response,
+              `Pending Transaction proceed as Transaction`,
+            )
+          }
+          return createEntityMessage(response, `Pending Transaction Approved`)
+        })
         .catch((err) => {
+          console.log(err)
           throw new InternalServerErrorException(err)
         })
-      return returner
-
       return returner
     } catch (err) {
       throw new BadRequestException(err)
     }
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('cancel-transaction/:id')
+  async cancelTransaction(
+    @Param()
+    pendingTransaction: CancelPendingTransactionDto,
+  ) {
+    return this.pendingTransactionService
+      .cancelPendingTransaction(pendingTransaction.id)
+      .then((res) =>
+        createEntityMessage(
+          res,
+          `Pending Transaction ${pendingTransaction.id} Cancelled`,
+        ),
+      )
+      .catch((err) => {
+        console.log(err)
+
+        throw new BadRequestException(err.message)
+      })
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('deny-transaction/:id')
+  async denyTransaction(
+    @Param()
+    pendingTransaction: CancelPendingTransactionDto,
+  ) {
+    return this.pendingTransactionService
+      .cancelPendingTransaction(pendingTransaction.id)
+      .then((res) =>
+        createEntityMessage(
+          res,
+          `Pending Transaction ${pendingTransaction.id} Denied`,
+        ),
+      )
+      .catch((err) => {
+        console.log(err)
+
+        throw new BadRequestException(err.message)
+      })
   }
 }

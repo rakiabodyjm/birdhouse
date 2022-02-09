2b11f73e720b52a1b4ef66a7262dc7ab5bd2a499
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-02-09 14:45:02 +0800

    Pending Transactions API

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -60,6 +60,7 @@ export class TransactionController {
         await this.transactionService
           .newVerifyIfApprovalNeeded(inventory_from, buyer)
           .then((res) => {
+            console.log('approvals', res)
             if (res) {
               return Object.keys(res).reduce((acc, caesarType) => {
                 return {
@@ -73,11 +74,11 @@ export class TransactionController {
           })
       if (approvals) {
         const inventory = await this.inventoryService.findOne(inventory_from)
-
         const caesar_buyer = await this.caesarService.findOne(buyer)
         /**
          * generate unique purchase_id
          */
+        console.log(caesar_buyer)
         const pending_purchase_id = v4()
         return {
           pending_transactions: await Promise.all(

6fb586695bd6d4cee7984a41484d845a79668bab
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-03-01 16:32:59 +0800

    Admin search feature

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -6,19 +6,20 @@ import { Admin } from 'src/admin/entities/admin.entity'
 import { Paginated } from 'src/types/Paginated'
 import { UserTypesAndUser } from 'src/types/Roles'
 import paginateFind from 'src/utils/paginate'
-import { Repository } from 'typeorm'
+import { Like, Repository } from 'typeorm'
 import { CreateAdminDto } from './dto/create-admin.dto'
 import { UpdateAdminDto } from './dto/update-admin.dto'
 
 @Injectable()
 export class AdminService {
+  relationsToLoad: UserTypesAndUser[]
+
   constructor(
     @InjectRepository(Admin)
     private adminRepository: Repository<Admin>,
   ) {
     this.relationsToLoad = ['user']
   }
-  relationsToLoad: UserTypesAndUser[]
 
   async create(createAdminDto: CreateAdminDto) {
     const newAdmin = this.adminRepository.create(createAdminDto)
@@ -78,5 +79,31 @@ export class AdminService {
     const deleteResult = await this.adminRepository.delete(id)
     console.log('admin delete result', deleteResult)
     return admin
+  }
+
+  search(queryString: string) {
+    return this.adminRepository
+      .find({
+        where: [
+          {
+            name: Like(`%${queryString}%`),
+          },
+          {
+            user: {
+              first_name: Like(`&${queryString}&`),
+            },
+          },
+          {
+            user: {
+              last_name: Like(`&${queryString}&`),
+            },
+          },
+        ],
+        take: 100,
+        relations: this.relationsToLoad,
+      })
+      .catch((err) => {
+        throw err
+      })
   }
 }

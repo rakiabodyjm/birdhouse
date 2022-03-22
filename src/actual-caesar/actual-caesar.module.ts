75a64c5bfb34eec7cd698ae00d6cef5dcaa488fa
Janeo Miguel Tiongson <33659063+rakiabodyjm@users.noreply.github.com>
2022-03-23 00:06:39 +0800

    Add ActualCaesarModule in AppModule

---
+++ src/actual-caesar/actual-caesar.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common'
+import { ActualCaesarService } from './actual-caesar.service'
+import { ActualCaesarController } from './actual-caesar.controller'
+import { HttpModule } from '@nestjs/axios'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
+@Module({
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        baseURL: configService.get('CAESAR_HOST'),
+        params: {
+          api_key: 'cs_b7c524cd0882fb32333af561faf8519d',
+          url: 'telco.com',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [ActualCaesarController],
+  providers: [ActualCaesarService],
+  exports: [ActualCaesarService],
+})
+export class ActualCaesarModule {}

ad54d8697edaf6e70ac3fdb889958f15f0b8ed74
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-04 13:03:52 +0800

    Make description field in CreateCaesarBankDto NoDuplicateInDB

--- src/cash-transfer/dto/caesar-bank/create-caesar-bank.dto.ts
+++ src/cash-transfer/dto/caesar-bank/create-caesar-bank.dto.ts
@@ -1,3 +1,5 @@
+import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
+import { NoDuplicateInDb } from 'src/pipes/validation/NoDuplicateInDb'
 import { IsOptional } from 'class-validator'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { Bank } from 'src/cash-transfer/entities/bank.entity'
@@ -14,6 +16,9 @@ export class CreateCaesarBankDto {
   })
   caesar: any
 
+  @NoDuplicateInDb(CaesarBank, 'description', {
+    message: `Caesar's Bank description already exists`,
+  })
   @IsOptional()
   description: string
 }

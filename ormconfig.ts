9b9a09f79bb80c0e71d3d6cc7b370a049a65df0b
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-06 03:38:29 +0800

    Fix ormconfig for db Migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,40 +1,31 @@
-// import { config } from 'dotenv'
-import { ConfigService } from '@nestjs/config'
+import { config } from 'dotenv'
 import { SqlServerConnectionOptions } from 'typeorm/driver/sqlserver/SqlServerConnectionOptions'
 
-// config({
-//   path:
-//     process.env.NODE_ENV === 'production'
-//       ? './.env.production'
-//       : './.env.development',
-// })
-
-const SQLConfig: (
-  configService: ConfigService,
-) => SqlServerConnectionOptions & {
+config({
+  path:
+    process.env.NODE_ENV === 'production'
+      ? './.env.production'
+      : './.env.development.local',
+})
+const SQLConfig: SqlServerConnectionOptions & {
   options: { trustServerCertificate: boolean }
-} = (configService: ConfigService) => {
-  return {
-    type: 'mssql',
-    host: configService.get('SQL_SERVER_HOST'),
-    port: Number(configService.get('SQL_SERVER_PORT')),
-    username: configService.get('SQL_SERVER_USERNAME'),
-    password: configService.get('SQL_SERVER_PASSWORD'),
-    database: configService.get('SQL_SERVER_DATABASE'),
-    synchronize: true,
-    options: {
-      trustServerCertificate: true,
-      // process.env.NODE_ENV === 'development' ||
-      // process.env.NODE_ENV === 'test' ||
-      // false,
-    },
-    entities: ['dist/src/**/*.entity{.ts,.js}'],
-    migrations: ['dist/db/migrations/*.js'],
-    cli: {
-      migrationsDir: 'db/migrations',
-    },
-    logging: false,
-  }
+} = {
+  type: 'mssql',
+  host: process.env.SQL_SERVER_HOST,
+  port: Number(process.env.SQL_SERVER_PORT),
+  username: process.env.SQL_SERVER_USERNAME,
+  password: process.env.SQL_SERVER_PASSWORD,
+  database: process.env.SQL_SERVER_DATABASE,
+  synchronize: true,
+  options: {
+    trustServerCertificate: true,
+  },
+  entities: ['dist/src/**/*.entity{.ts,.js}'],
+  migrations: ['dist/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'db/migrations',
+  },
+  logging: false,
 }
 // const SQLConfig: SqlServerConnectionOptions & {
 //   options: { trustServerCertificate: boolean }

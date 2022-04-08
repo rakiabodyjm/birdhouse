c13c5eaec4ffd6bcbadb08b38fad5a4608c53911
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-09 03:55:36 +0800

    GetAllCashTransfer DTO

--- src/cash-transfer/dto/cash-transfer/get-all-cash-transfer.dto.ts
+++ src/cash-transfer/dto/cash-transfer/get-all-cash-transfer.dto.ts
@@ -1,7 +1,8 @@
 import { PartialType } from '@nestjs/swagger'
-import { IsOptional } from 'class-validator'
+import { IsEnum, IsOptional } from 'class-validator'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
+import { CashTransferAs } from 'src/cash-transfer/entities/cash-transfer.entity'
 import { TransferType } from 'src/cash-transfer/entities/transfer-type.entity'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 import { PaginateOptions } from 'src/types/Paginated'
@@ -13,17 +14,45 @@ export class GetAllCashTransferDto extends PartialType(PaginateOptions) {
   @IsOptional()
   caesar?: Caesar['id']
 
-  @ExistsInDb(TransferType, 'id', {
-    message: `Transfer Type doesn't exist`,
-  })
   @IsOptional()
-  transfer_type?: TransferType['id']
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar Bank Entity doesn't exist`,
+  })
+  caesar_bank: CaesarBank['id']
+
+  // @ExistsInDb(TransferType, 'id', {
+  //   message: `Transfer Type doesn't exist`,
+  // })
+  // @IsOptional()
+  // transfer_type?: TransferType['id']
+
+  @IsEnum(CashTransferAs)
+  @IsOptional()
+  as: CashTransferAs
 
   @ExistsInDb(CaesarBank, 'id', {
     message: `Caesar bank account doesn't exist`,
   })
   @IsOptional()
-  caesar_bank?: CaesarBank['id']
+  caesar_bank_from?: CaesarBank['id']
+
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar bank account doesn't exist`,
+  })
+  @IsOptional()
+  caesar_bank_to?: CaesarBank['id']
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar TO doesn't exist`,
+  })
+  to?: Caesar['id']
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar FROM doesn't exist`,
+  })
+  from?: Caesar['id']
 
   @IsOptional()
   date_from?: Date

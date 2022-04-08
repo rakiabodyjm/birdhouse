b2b9009d23b6aa1bc82d828d5d4900b3c163d060
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-09 03:54:39 +0800

    Finalize Cash Transfer with relationships

--- src/cash-transfer/cash-transfer.service.spec.ts
+++ src/cash-transfer/cash-transfer.service.spec.ts
@@ -5,7 +5,10 @@ import { CaesarService } from 'src/caesar/caesar.service'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { Bank } from 'src/cash-transfer/entities/bank.entity'
 import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
-import { CashTransfer } from 'src/cash-transfer/entities/cash-transfer.entity'
+import {
+  CashTransfer,
+  CashTransferAs,
+} from 'src/cash-transfer/entities/cash-transfer.entity'
 import { TransferType } from 'src/cash-transfer/entities/transfer-type.entity'
 import { CaesarBankService } from 'src/cash-transfer/services/caesar-bank.service'
 import { TransferTypeService } from 'src/cash-transfer/services/transfer-type.service'
@@ -97,6 +100,7 @@ const mockCaesarData: (Partial<Caesar> & { id: string })[] = [
     admin: {
       name: 'Admin 1',
     } as Admin,
+    cash_transfer_balance: 0,
   },
   {
     id: 'caesar2',
@@ -104,18 +108,19 @@ const mockCaesarData: (Partial<Caesar> & { id: string })[] = [
     subdistributor: {
       name: 'Subdistributor 1',
     } as Subdistributor,
+    cash_transfer_balance: 0,
   },
 ]
 
 let mockCaesarBankData: (Partial<CaesarBank> & { id: number | string })[] = [
   {
-    id: 1,
-    balance: 10000,
+    id: '1',
+    balance: 0,
     bank: idFinder(1, mockBankData) as Bank,
     caesar: idFinder('caesar1', mockCaesarData) as Caesar,
   },
   {
-    id: 2,
+    id: '2',
     balance: 0,
     bank: idFinder(2, mockBankData) as Bank,
     caesar: idFinder('caesar2', mockCaesarData) as Caesar,
@@ -136,9 +141,12 @@ describe('CashTransferService', () => {
             async findOne(id) {
               return idFinder(id, mockCaesarBankData)
             },
-            async pay(caesarBankId: CaesarBank | number, amount: number) {
+            async pay(
+              caesarBankId: CaesarBank | CaesarBank['id'],
+              amount: number,
+            ) {
               const caesarBank = (
-                typeof caesarBankId === 'number'
+                typeof caesarBankId === 'string'
                   ? idFinder(caesarBankId, mockCaesarBankData)
                   : { ...caesarBankId }
               ) as CaesarBank
@@ -178,6 +186,36 @@ describe('CashTransferService', () => {
             async findOne(id) {
               return idFinder(id, mockCaesarData)
             },
+            async payCashTransferBalance(
+              caesar: Caesar | Caesar['id'],
+              amount: number,
+            ) {
+              let currentCaesar: Partial<Caesar>
+              if (typeof caesar === 'string') {
+                // currentCaesar = await this.findOne(caesar)
+                currentCaesar = await mockCaesarData.find(
+                  (ea) => ea.id === caesar,
+                )
+              } else {
+                currentCaesar = caesar
+              }
+              let index: number
+              mockCaesarData.some((ea, i) => {
+                if (ea.id === currentCaesar.id) {
+                  index = i
+                  return true
+                }
+                return false
+              })
+
+              const copyMockCaesar = [...mockCaesarData]
+              copyMockCaesar[index] = {
+                ...copyMockCaesar[index],
+                cash_transfer_balance:
+                  copyMockCaesar[index].cash_transfer_balance + amount,
+              }
+              mockCaesarData == copyMockCaesar
+            },
           },
         },
         {
@@ -196,6 +234,24 @@ describe('CashTransferService', () => {
           },
           // useClass: Repository,
         },
+        {
+          provide: getRepositoryToken(TransferType),
+          useValue: {
+            async findOneOrFail(field: {
+              [x: string | number | symbol]: unknown
+            }) {
+              // return idFinder(id, mockTransferTypeData)
+
+              return mockTransferTypeData.find((ea) => {
+                const entriesTuple = Object.entries(field)
+                const key = entriesTuple[0]
+                const value = entriesTuple[1]
+
+                return ea[`${key}`] === value
+              })
+            },
+          },
+        },
       ],
     }).compile()
 
@@ -206,15 +262,35 @@ describe('CashTransferService', () => {
     expect(service).toBeDefined()
   })
 
-  it('[Transfer] with sufficient balance', async () => {
-    const caesar_bank_from = 1
-    const caesar_bank_to = 2
+  it.only('[Deposit] with Initial Balance of 10,000', async () => {
+    await service
+      .deposit({
+        from: 'caesar1',
+        caesar_bank_to: '1',
+        as: CashTransferAs.DEPOSIT,
+        description: `Initial Deposit`,
+        amount: 10000,
+        transfer_type: 'deposit',
+        bank_fee: 0,
+      })
+      .then((res) => {
+        expect(res).toBeDefined()
+      })
+  })
+
+  it.only('[Transfer] with sufficient balance 100', async (done) => {
+    const caesar_bank_from = '1'
+    const caesar_bank_to = '2'
     await service
       .transfer({
         amount: 100,
         caesar_bank_from,
         caesar_bank_to,
-        transfer_type: 2,
+        // transfer_type: 2,
+        bank_fee: 25,
+        description: `Test Transfer with sufficient balance`,
+        transfer_type: 'transfer',
+        as: CashTransferAs.TRANSFER,
       })
       .then((res) => {
         expect(res).toBeDefined()
@@ -230,33 +306,46 @@ describe('CashTransferService', () => {
             .balance,
         ).toEqual(9900)
       })
+      .catch((err) => {
+        expect(err).not.toBeDefined()
+      })
+      .finally(() => {
+        done()
+      })
   })
 
-  it('[Transfer] if insufficient balance with error', async () => {
-    const caesar_bank_from = 1
-    const caesar_bank_to = 2
-    await service
-      .transfer({
-        amount: 11000,
-        caesar_bank_from,
-        caesar_bank_to,
-        transfer_type: 2,
-      })
-      .catch((err) => {
-        expect(err).toBeDefined()
-      })
-      .then((res) => {
-        expect(res).not.toBeDefined()
-      })
+  it('[Transfer] if insufficient balance with error', async (done) => {
+    const caesar_bank_from = '1'
+    const caesar_bank_to = '2'
+    await expect(
+      service
+        .transfer({
+          amount: 11000,
+          caesar_bank_from,
+          caesar_bank_to,
+          transfer_type: 'transfer',
+          description: 'Transfer with insufficient balance',
+          as: CashTransferAs.TRANSFER,
+        })
+        .catch((err) => {
+          expect(err.message).toBeDefined()
+        })
+        .then((res) => {
+          expect(res).not.toBeDefined()
+        }),
+    ).rejects.toBeDefined()
   })
 
   it('[Withdraw] Successfully if sufficient balance', async (done) => {
-    service
+    await service
       .withdraw({
         amount: 9900,
-        caesar_bank_from: 1,
+        caesar_bank_from: '1',
         to: idFinder('caesar1', mockCaesarData).id,
-        transfer_type: idFinder(2, mockTransferTypeData).id,
+        transfer_type: 'withdraw',
+        description:
+          'Withdraw successfully with sufficient balance from caesar_bank_from',
+        as: CashTransferAs.WITHDRAW,
       })
       .then((cashTransfer) => {
         expect(cashTransfer.amount).toEqual(-9900)
@@ -265,43 +354,39 @@ describe('CashTransferService', () => {
         const csr = idFinder(1, mockCaesarBankData)
         expect(csr.balance).toEqual(0)
       })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-        done()
-      })
   })
 
   it('[Withdraw] if insufficient balance with error', async (done) => {
-    service
-      .withdraw({
-        amount: 1,
-        caesar_bank_from: 1,
-        to: idFinder('caesar1', mockCaesarData).id,
-        transfer_type: idFinder(2, mockTransferTypeData).id,
-      })
-      .then((cashTransfer) => {
-        expect(cashTransfer).not.toBeDefined()
-        // expect(cashTransfer.amount).toEqual(-9900)
-      })
-      .catch((err) => {
-        expect(err).toBeDefined()
-      })
-      .finally(() => {
-        done()
-      })
+    expect(
+      service
+        .withdraw({
+          amount: 1,
+          caesar_bank_from: '1',
+          to: idFinder('caesar1', mockCaesarData).id,
+          // transfer_type: idFinder(2, mockTransferTypeData).id,
+          transfer_type: 'withdraw',
+          description: `Withdraw with insufficient balance with error`,
+          as: CashTransferAs.WITHDRAW,
+        })
+        .then((cashTransfer) => {
+          expect(cashTransfer).not.toBeDefined()
+          // expect(cashTransfer.amount).toEqual(-9900)
+        })
+        .catch((err) => {
+          expect(err.message).toBeDefined()
+        }),
+    ).rejects
   })
 
   it('[Deposit] successfully', async (done) => {
-    service
+    await service
       .deposit({
         amount: 1000,
-        caesar_bank_to: 1,
-        // from: idFinder(2, mockTransferTypeData).id,
+        caesar_bank_to: '1',
         from: customFinder(['id', 'caesar1'], mockCaesarData).id,
-        transfer_type: customFinder(['name', 'DEPOSIT'], mockTransferTypeData)
-          .id,
+        transfer_type: 'deposit',
+        description: `Deposit succesfully with sufficient balance`,
+        as: CashTransferAs.DEPOSIT,
       })
       .then((res) => {
         expect(res.amount).toBe(1000)
@@ -309,8 +394,42 @@ describe('CashTransferService', () => {
       .then(() => {
         expect(idFinder(1, mockCaesarBankData).balance).toBe(1000)
       })
-      .finally(() => {
-        done()
-      })
+  })
+
+  it('Should all be balanced', () => {
+    const withdrawAmounts = cashTransfersGenerated
+      .filter((ea) => ea.as === 'WITHDRAW')
+      .reduce((acc, ea) => {
+        return acc + ea.amount
+      }, 0)
+    console.log('WITHDRAWN', withdrawAmounts)
+    const depositAmounts = cashTransfersGenerated
+      .filter((ea) => ea.as === 'DEPOSIT')
+      .reduce((acc, ea) => {
+        return acc + ea.amount
+      }, 0)
+
+    console.log('DEPOSITED', depositAmounts)
+
+    const caesarBalances = mockCaesarData.reduce(
+      (acc, ea) => acc + ea.cash_transfer_balance,
+      0,
+    )
+    console.log('CAESAR BALANCES', caesarBalances)
+
+    const bankBalances = mockCaesarBankData.reduce(
+      (acc, ea) => acc + ea.balance,
+      0,
+    )
+    console.log('BANK BALANCES', bankBalances)
+
+    expect(
+      cashTransfersGenerated.reduce((acc, ea) => {
+        return acc + ea.amount
+      }, 0),
+    ).toBe(depositAmounts - withdrawAmounts)
+  })
+  afterAll(() => {
+    console.log('cashTransfersGenerated', cashTransfersGenerated)
   })
 })
--- src/cash-transfer/entities/cash-transfer.entity.ts
+++ src/cash-transfer/entities/cash-transfer.entity.ts
@@ -1,13 +1,15 @@
 import { IntersectionType } from '@nestjs/swagger'
+import { Expose } from 'class-transformer'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
-import { TransferType } from 'src/cash-transfer/entities/transfer-type.entity'
 import {
   Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm'
@@ -18,33 +20,39 @@ export enum CashTransferAs {
   WITHDRAW = 'WITHDRAW',
   LOAN = 'LOAN',
   'LOAN PAYMENT' = 'LOAN PAYMENT',
-  INTEREST = 'INTEREST',
-  'BANK FEES' = 'BANK FEES',
+  // INTEREST = 'INTEREST',
+  // 'BANK FEES' = 'BANK FEES',
 }
 
 export class CashTransferWithDraw {
+  @JoinColumn()
   @ManyToOne(
     (type) => CaesarBank,
     (caesarBank) => caesarBank.cash_transfer_from,
   )
   caesar_bank_from: CaesarBank
 
+  @JoinColumn()
   @ManyToOne(() => Caesar, (caesar) => caesar.cash_transfer_to)
   to: Caesar
 }
 
 export class CashTransferDeposit {
+  @JoinColumn()
   @ManyToOne(() => CaesarBank, (caesarBank) => caesarBank.cash_transfer_to)
   caesar_bank_to: CaesarBank
 
+  @JoinColumn()
   @ManyToOne(() => Caesar, (caesar) => caesar.cash_transfer_from)
   from: Caesar
 }
 
-class CashTransferType {
+export class CashTransferType {
+  @JoinColumn()
   @ManyToOne(() => CaesarBank, (caesarBank) => caesarBank.cash_transfer_to)
   caesar_bank_to: CaesarBank
 
+  @JoinColumn()
   @ManyToOne(() => CaesarBank, (caesarBank) => caesarBank.cash_transfer_from)
   caesar_bank_from: CaesarBank
 }
@@ -57,19 +65,26 @@ export class CashTransfer extends IntersectionType(
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @ManyToOne(
-    (type) => TransferType,
-    (transferType) => transferType.cash_transfer,
-  )
-  transfer_type: TransferType
+  // @JoinColumn({
+  //   name: 'transfer_type_id',
+  // })
+  // @ManyToOne(
+  //   (type) => TransferType,
+  //   (transferType) => transferType.cash_transfer,
+  // )
+  // transfer_type: TransferType
 
   @Column()
   amount: number
 
+  // @Column({
+  //   default: CashTransferAs.TRANSFER,
+  // })
+  // tag: CashTransferAs
   @Column({
     default: CashTransferAs.TRANSFER,
   })
-  tag: CashTransferAs
+  as: CashTransferAs
 
   @CreateDateColumn({
     type: 'datetime',
@@ -89,8 +104,167 @@ export class CashTransfer extends IntersectionType(
 
   @Column({
     type: 'text',
+    nullable: true,
   })
   description: string
 
-  // @ManyToOne(()=> Caesar, caesar => caesar.loans)
+  @JoinColumn({
+    name: 'caesar_bank_to',
+  })
+  @ManyToOne(() => CaesarBank, (caesarBank) => caesarBank.cash_transfer_to)
+  caesar_bank_to: CaesarBank
+
+  @JoinColumn({
+    name: 'caesar_bank_from',
+  })
+  @ManyToOne(() => CaesarBank, (caesarBank) => caesarBank.cash_transfer_from)
+  caesar_bank_from: CaesarBank
+
+  @JoinColumn({
+    name: 'caesar_to',
+  })
+  @ManyToOne(() => Caesar, (caesar) => caesar.cash_transfer_to)
+  to: Caesar
+
+  @JoinColumn({
+    name: 'caesar_from',
+  })
+  @ManyToOne(() => Caesar, (caesar) => caesar.cash_transfer_from)
+  from: Caesar
+
+  @Column({
+    default: 0,
+  })
+  bank_charge: number
+
+  // @Column({
+  //   default: null,
+  // })
+  // interest?: number
+
+  @Column({
+    default: 0,
+  })
+  remaining_balance_from: number
+
+  @Column({ default: 0 })
+  remaining_balance_to: number
+
+  // @OneToOne(() => Loan, (l) => l.cash_transfer_reference)
+  // loan_id: Loan
+
+  // @JoinColumn({
+  //   name: 'loan_payment_id',
+  // })
+  // @ManyToOne(() => Loan, (loan) => loan.payments)
+  // loan_payment_id: Loan
+  // // @Expose()
+  // // interest_amount() {
+  // //   if (this.as === CashTransferAs.LOAN) {
+  // //     return this.amount + 0.01 * this.amount
+  // //   }
+  // // }
+
+  // // @JoinColumn({
+  // //   name: 'loan_id',
+  // // })
+  // // @ManyToOne(() => Loan, (loan) => loan.payments)
+  // // loan: Loan
+
+  // // @
+
+  @JoinColumn({
+    name: 'loan_id',
+  })
+  @ManyToOne(() => CashTransfer, (ct) => ct.payments)
+  loan: CashTransfer
+
+  @OneToMany(() => CashTransfer, (ct) => ct.loan)
+  payments: CashTransfer[]
+
+  @Expose()
+  interest() {
+    if (this.as !== CashTransferAs.LOAN) {
+      return null
+    }
+    let interestRate = 0
+    const dateNow = new Date(Date.now())
+    const dateLoan = new Date(this.created_at)
+
+    const whichShiftFrom: 'first' | 'second' =
+      dateLoan.getHours() > 0 && dateLoan.getHours() < 14 ? 'first' : 'second'
+
+    const whichShiftTo: 'first' | 'second' =
+      dateNow.getHours() > 0 && dateNow.getHours() < 14 ? 'first' : 'second'
+
+    const dayFrom = new Date(
+      `${
+        monthNames[dateLoan.getMonth()]
+      } ${dateLoan.getDate()}, ${dateLoan.getFullYear()}`,
+    )
+    const dayTo = new Date(
+      `${
+        monthNames[dateNow.getMonth()]
+      } ${dateNow.getDate()}, ${dateLoan.getFullYear()}`,
+    )
+
+    const dayDiff = (dayTo.getTime() - dayFrom.getTime()) / (1000 * 3600 * 24)
+
+    const firstDayCount = whichShiftFrom === 'second' ? 0.5 : 1
+
+    const lastDayCount = whichShiftTo === 'second' ? 1 : 0.5
+
+    if (dayDiff >= 2) {
+      interestRate += firstDayCount + lastDayCount
+      interestRate += (dayDiff - 1) * 1
+    }
+    if (dayDiff <= 1) {
+      interestRate += firstDayCount + lastDayCount
+    }
+
+    return Number(interestRate.toFixed(4))
+  }
+
+  @Expose()
+  total_amount() {
+    const amount = this.amount || 0
+    const bank_charge = this.bank_charge || 0
+    const interest = this.interest() || 0
+
+    return (
+      amount +
+      bank_charge +
+      (this.as === CashTransferAs.LOAN ? (interest / 100) * amount : 0)
+    )
+  }
+  loan_total() {
+    if (this.as === CashTransferAs.LOAN) {
+      return this.amount + this.amount * this.interest()
+    }
+    return null
+    // if (this.as === CashTransferAs.LOAN) {
+    //   const totalPaymentsDone =
+    //     this.payments?.reduce((acc, ea) => {
+    //       return acc + ea.amount
+    //     }, 0) || 0
+    //   const loanTotal = this.amount - totalPaymentsDone
+    //   return loanTotal
+    // }
+    // return null
+  }
 }
+
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]

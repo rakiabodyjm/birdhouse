75edb839cc5f2063bf9d2419a4b31ce4e3291a9a
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-10 03:58:47 +0800

    CreateCashTransferDto

--- src/cash-transfer/dto/cash-transfer/create-cash-transfer.dto.ts
+++ src/cash-transfer/dto/cash-transfer/create-cash-transfer.dto.ts
@@ -1,40 +1,107 @@
-import { IsEnum, IsNumber, IsOptional } from 'class-validator'
+import { IntersectionType } from '@nestjs/mapped-types'
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional } from 'class-validator'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
 import { CashTransferAs } from 'src/cash-transfer/entities/cash-transfer.entity'
-import { TransferType } from 'src/cash-transfer/entities/transfer-type.entity'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 
-export class CreateCashTransferDto {
-  @ExistsInDb(TransferType, 'id', {
-    message: `Transfer Type doesn't exist`,
-  })
-  //   transfer_type: TransferType['id']
-  transfer_type: any
-
-  @ExistsInDb(CaesarBank, 'id', {
-    message: `Caesar's bank Account doesn't exist`,
-  })
-  //   caesar_bank: CaesarBank['id']
-  caesar_bank: any
-
-  @IsNumber()
-  amount: number
-
+export class DepositCashTransfer {
+  @IsOptional()
   @ExistsInDb(Caesar, 'id', {
     message: `Caesar Doesn't exist`,
   })
   from: any
 
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar's bank Account FROM doesn't exist`,
+  })
+  caesar_bank_to: any
+}
+
+export class WithdrawCashTrashTransfer {
+  @IsOptional()
   @ExistsInDb(Caesar, 'id', {
     message: `Caesar Doesn't exist`,
   })
   to: any
 
   @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar's bank Account TO doesn't exist`,
+  })
+  caesar_bank_from: any
+}
+
+export class TransferCashTransfer {
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar's bank Account FROM doesn't exist`,
+  })
+  caesar_bank_to: any
+
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar's bank Account TO doesn't exist`,
+  })
+  caesar_bank_from: any
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar's Account TO doesn't exist`,
+  })
+  to: any
+}
+
+export class LoanCashTransfer {
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `LOAN | Caesar bank from doesn't exist `,
+  })
+  caesar_bank_from: any
+
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `LOAN | Caesar bank to doesn't exist`,
+  })
+  caesar_bank_to: any
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `LOAN | Caesar to doesn't exist`,
+  })
+  to: any
+}
+export class GenericCashTransfer {
+  @IsNotEmpty({
+    message: `Amount is required`,
+  })
+  @IsNumber()
+  amount: number
+
+  @IsOptional()
   description: string
 
-  @IsEnum(CashTransferAs)
+  @IsEnum(CashTransferAs, {
+    message: `Transaction Type AS required`,
+  })
+  // @IsOptional()
+  @IsNotEmpty({
+    message: `Transaction Type AS should not be empty`,
+  })
+  as: CashTransferAs
+
   @IsOptional()
-  tag?: CashTransferAs
+  bank_fee: number
+
+  @IsOptional()
+  interest: number
 }
+
+export class CreateCashTransferDto extends IntersectionType(
+  IntersectionType(
+    IntersectionType(WithdrawCashTrashTransfer, DepositCashTransfer),
+    IntersectionType(TransferCashTransfer, GenericCashTransfer),
+  ),
+  LoanCashTransfer,
+) {}

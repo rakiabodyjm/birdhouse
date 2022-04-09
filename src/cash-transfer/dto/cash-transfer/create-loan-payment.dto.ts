be8c81bd73097dc95fcedeb3920059b5e8c9208a
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-10 03:58:35 +0800

    CreateLoanPaymentDto

---
+++ src/cash-transfer/dto/cash-transfer/create-loan-payment.dto.ts
@@ -0,0 +1,40 @@
+import { Caesar } from 'src/caesar/entities/caesar.entity'
+import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
+import { Transform } from 'class-transformer'
+import { IsNotEmpty, IsOptional, Min, Validate } from 'class-validator'
+import { CashTransfer } from 'src/cash-transfer/entities/cash-transfer.entity'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
+
+export class CreateLoanPaymentDto {
+  @ExistsInDb(CashTransfer, 'id', {
+    message: `Loan doesn't exist`,
+  })
+  id: any
+
+  @IsNotEmpty()
+  amount: number
+
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar Account TO not found`,
+  })
+  @IsOptional()
+  to?: Caesar['id']
+
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar Account TO bank doesn't exist`,
+  })
+  caesar_bank_to?: CaesarBank['id']
+
+  @IsOptional()
+  @ExistsInDb(CaesarBank, 'id', {
+    message: `Caesar Account FROM bank doesn't exist`,
+  })
+  caesar_bank_from?: any
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar Account FROM not found`,
+  })
+  from: Caesar['id']
+}

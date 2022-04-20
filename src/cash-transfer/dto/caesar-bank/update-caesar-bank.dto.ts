71ab42b371c7a6c2f7edc0596d80e5e86ba507d0
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-20 16:57:17 +0800

    UpdateCaesarBankDto caesar and bank fields

--- src/cash-transfer/dto/caesar-bank/update-caesar-bank.dto.ts
+++ src/cash-transfer/dto/caesar-bank/update-caesar-bank.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from '@nestjs/swagger'
-import { CreateCaesarBankDto } from 'src/cash-transfer/dto/caesar-bank/create-caesar-bank.dto'
+import { Caesar } from 'src/caesar/entities/caesar.entity'
+import { Bank } from 'src/cash-transfer/entities/bank.entity'
+import { IsOptional } from 'class-validator'
+import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 
-export class UpdateCaesarBankDto extends PartialType(CreateCaesarBankDto) {}
+export class UpdateCaesarBankDto {
+  @IsOptional()
+  @ExistsInDb(Bank, 'id', {
+    message: `Bank doesn't exist`,
+  })
+  bank?: any
+
+  @IsOptional()
+  @ExistsInDb(Caesar, 'id', {
+    message: `Caesar doesn't exist`,
+  })
+  caesar?: any
+}

e89e8f21115d865b9f444ba683be6ce8c6627485
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-04-23 14:40:51 +0800

    UpdateCaesarBankDto account_number

--- src/cash-transfer/dto/caesar-bank/update-caesar-bank.dto.ts
+++ src/cash-transfer/dto/caesar-bank/update-caesar-bank.dto.ts
@@ -1,18 +1,28 @@
+import { CaesarBank } from './../../entities/caesar-bank.entity'
+import { NoDuplicateInDb } from 'src/pipes/validation/NoDuplicateInDb'
+import { PartialType } from '@nestjs/swagger'
+import { CreateCaesarBankDto } from './create-caesar-bank.dto'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { Bank } from 'src/cash-transfer/entities/bank.entity'
 import { IsOptional } from 'class-validator'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
 
-export class UpdateCaesarBankDto {
-  @IsOptional()
+export class UpdateCaesarBankDto extends PartialType(CreateCaesarBankDto) {
   @ExistsInDb(Bank, 'id', {
     message: `Bank doesn't exist`,
   })
+  @IsOptional()
   bank?: any
 
-  @IsOptional()
   @ExistsInDb(Caesar, 'id', {
     message: `Caesar doesn't exist`,
   })
+  @IsOptional()
   caesar?: any
+
+  @NoDuplicateInDb(CaesarBank, 'description', {
+    message: `Caesar's Bank description already exists`,
+  })
+  @IsOptional()
+  description?: string
 }

cdc41866a957977e4b4b2be6a245ebb028870b92
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-04 01:32:47 +0800

    Create CashTransfer maintenance endpoints (backup, restore, get-restore-files)

--- src/cash-transfer/controllers/cash-transfer.controller.ts
+++ src/cash-transfer/controllers/cash-transfer.controller.ts
@@ -17,15 +17,33 @@ import { CreateCashTransferDto } from 'src/cash-transfer/dto/cash-transfer/creat
 import { UpdateCashTransferDto } from 'src/cash-transfer/dto/cash-transfer/update-cash-transfer.dto'
 import { GetAllCashTransferDto } from 'src/cash-transfer/dto/cash-transfer/get-all-cash-transfer.dto'
 import { ErrorsInterceptor } from 'src/interceptors/error.interceptor'
+import { InjectRepository } from '@nestjs/typeorm'
+import { CashTransfer } from 'src/cash-transfer/entities/cash-transfer.entity'
+import { Repository } from 'typeorm'
+import * as fs from 'fs'
+import * as path from 'path'
 
 @Controller('cash-transfer')
 @UseInterceptors(ErrorsInterceptor, ClassSerializerInterceptor)
 export class CashTransferController {
-  constructor(private readonly cashTransferService: CashTransferService) {}
+  constructor(
+    private readonly cashTransferService: CashTransferService,
+    @InjectRepository(CashTransfer)
+    private cashTransferRepository: Repository<CashTransfer>,
+  ) {}
 
   @Get()
   findAll(@Query() getAllCashTransfer: GetAllCashTransferDto) {
     return this.cashTransferService.findAll(getAllCashTransfer)
+  }
+
+  @Get('get-backup-files')
+  getBackupFiles() {
+    const pathLoc = path.resolve(process.cwd(), 'data', 'cash-transfer')
+
+    const files = fs.readdirSync(pathLoc)
+
+    return files
   }
 
   @Get(':id')
@@ -90,5 +108,73 @@ export class CashTransferController {
   @Get('loan-payments/:id')
   getLoanPayments(@Param('id') cashTransferId: string) {
     return this.cashTransferService.getLoanPayments(cashTransferId)
+  }
+
+  @Post('backupData/:name?')
+  backupData(@Param('name') name?: string) {
+    return this.cashTransferRepository.find().then((res) => {
+      const pathLoc = path.resolve(process.cwd(), 'data', 'cash-transfer')
+      if (!fs.existsSync(pathLoc)) {
+        fs.mkdirSync(pathLoc)
+      }
+      return new Promise((resolve, rej) => {
+        fs.writeFile(
+          path.resolve(
+            pathLoc,
+            `cash-transfer-${new Date(Date.now()).getTime()}${
+              name ? `-${name}` : ''
+            }.json`,
+          ),
+          JSON.stringify(res),
+          {
+            encoding: 'utf-8',
+          },
+          (err) => {
+            if (err) {
+              rej(err)
+            } else {
+              resolve(`Success`)
+            }
+          },
+        )
+      })
+    })
+  }
+
+  @Post('restoreData/:name?')
+  restoreData(@Param('name') name?: string) {
+    const pathLoc = path.resolve(process.cwd(), 'data', 'cash-transfer')
+
+    // const files = fs.readdirSync(pathLoc)
+
+    // let selected:string|null=null
+    /**
+     * get latest
+     */
+    // files.filter(ea => {
+
+    //     const [, , , namae] = ea
+    //   if (namae && name && (name === namae)) {
+    //       return true
+    //   } else if(!name) {
+
+    //   }
+    //   return false
+    //   }).forEach(ea => {
+    //     const [cash, transfer, date, namae] = ea.split('-')
+
+    //     if(Number(date) > 0)
+    //   })
+
+    const read = fs.readFileSync(path.resolve(pathLoc, name), {
+      encoding: 'utf-8',
+    })
+
+    const cashTransfersToRestore: CashTransfer[] = JSON.parse(read)
+    return Promise.all(
+      cashTransfersToRestore.map(async (ea) => {
+        return await this.cashTransferRepository.save(ea)
+      }),
+    )
   }
 }

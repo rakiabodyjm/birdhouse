a63ff404c25e266135e6663302879019cfbaaab1
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-17 09:49:58 +0800

    CaesarService searchV2 commonQuery

--- src/caesar/dto/search-caesar.dto.ts
+++ src/caesar/dto/search-caesar.dto.ts
@@ -1,7 +1,11 @@
+import { UserTypesAndUser } from 'src/types/Roles'
 import { IsOptional } from 'class-validator'
 import { PaginateOptions } from 'src/types/Paginated'
 
 export class SearchCaesarDto extends PaginateOptions {
   @IsOptional()
   searchQuery: string
+
+  @IsOptional()
+  account_type: UserTypesAndUser
 }

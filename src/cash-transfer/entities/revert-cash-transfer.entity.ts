98a8a717c95d6bac1f51f353b5d155182e9aff9e
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-25 17:55:21 +0800

    Fix RevertCashTransfer table name from cash_trasnfer to cash_transfer

--- src/cash-transfer/entities/revert-cash-transfer.entity.ts
+++ src/cash-transfer/entities/revert-cash-transfer.entity.ts
@@ -13,7 +13,7 @@ export class RevertCashTransfer {
   @PrimaryGeneratedColumn('increment')
   id: number
 
-  @JoinColumn({ name: 'cash_trasnfer_id' })
+  @JoinColumn({ name: 'cash_transfer_id' })
   @OneToOne(() => CashTransfer, (ct) => ct.revert_cash_transfer)
   cash_transfer: CashTransfer
 

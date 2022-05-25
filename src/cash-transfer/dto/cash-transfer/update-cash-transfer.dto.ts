0547e7e4810aaf44ec369fefc73bebfa23304c4a
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-05-25 17:59:29 +0800

    UpdateCashTransfer message and description fields

--- src/cash-transfer/dto/cash-transfer/update-cash-transfer.dto.ts
+++ src/cash-transfer/dto/cash-transfer/update-cash-transfer.dto.ts
@@ -1,7 +1,4 @@
-import { CashTransfer } from 'src/cash-transfer/entities/cash-transfer.entity'
-import { PartialType } from '@nestjs/swagger'
 import { IsNumber, IsOptional } from 'class-validator'
-import { CreateCashTransferDto } from './create-cash-transfer.dto'
 
 export class UpdateCashTransferDto {
   @IsOptional()
@@ -9,5 +6,11 @@ export class UpdateCashTransferDto {
   override_interest?: number | null
 
   @IsOptional()
-  created_at: Date
+  created_at?: Date
+
+  @IsOptional()
+  message?: string
+
+  @IsOptional()
+  description?: string
 }

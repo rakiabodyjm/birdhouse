ec1774aaa32674de205522a9ff304518ceeee075
Miguel Tiongson <rakiabodyjm@gmail.com>
2022-06-02 05:55:21 +0800

    Loan transfer amonut validation message

--- src/cash-transfer/dto/cash-transfer/create-loan-transfer.dto.ts
+++ src/cash-transfer/dto/cash-transfer/create-loan-transfer.dto.ts
@@ -2,7 +2,13 @@ import { GenericCashTransfer } from './create-cash-transfer.dto'
 import { Caesar } from 'src/caesar/entities/caesar.entity'
 import { CaesarBank } from 'src/cash-transfer/entities/caesar-bank.entity'
 import { ExistsInDb } from 'src/pipes/validation/ExistsInDb'
-import { IsOptional, ValidateIf } from 'class-validator'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Min,
+  ValidateIf,
+} from 'class-validator'
 
 export class CreateLoanTransferDto extends GenericCashTransfer {
   @IsOptional()
@@ -27,9 +33,18 @@ export class CreateLoanTransferDto extends GenericCashTransfer {
   to: any
 
   @IsOptional()
-  @ValidateIf((o) => !o.caesar_bank_from || o.from)
+  @ValidateIf((o) => !o.caesar_bank_from || o.from, {
+    message: `caesar_bank_from already exists, remove from field`,
+  })
   @ExistsInDb(Caesar, 'id', {
     message: `LOAN | Caesar from doesn't exist`,
   })
   from: any
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1, {
+    message: `Must be at least 1 Peso`,
+  })
+  amount: number
 }

ecacc49b0d266479d701eade18be1ea4820fb073
Janeo Tiongson <rakiabodyjm@gmail.com>
2022-09-01 14:43:42 +0800

    Update dockerfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common'
+import { Public } from 'src/auth/decorators/public.decorator'
 import { AppService } from './app.service'
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
